17fdcd16a26c164ba82610e49a41d09c6c3ed90d
dra97 <dudikamaludin97@gmail.com>
2019-04-13 04:38:52 +0700

    change index with connect to db

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dynocodewebappserver.database.windows.net";
+    $user = "dynocode";
+    $pass = "24agustus.";
+    $db = "db_dynocode";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
